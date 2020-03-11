1c70eda871c0a7467d6895f2f37917c05d3cf3cf
DavidJWieczorek <55002237+DavidJWieczorek@users.noreply.github.com>
2020-03-11 11:41:16 -0400

    update part1

---
+++ week10/comments_in_source_code_part2.php
@@ -0,0 +1,21 @@
+<?php
+
+echo '<h1>This is an example of an H1 header tag</h1>';
+# Created by John Smith with employee id: 123456
+
+echo '<h2>Admin panel information</h2>';
+# The password for admin panel is: 123456
+
+echo '<h3>Hours worked on my project</h3>';
+# Useless project took 30 hours of my week
+
+echo '<h4>Project description</h4>';
+# I copy pasted all the code from stackoverflow :P
+
+echo '<h5>Exam content</h5>';
+# To be honest I haven't learned anything
+
+echo '<h6>Browser displaying page</h6>'
+# Will this display in the browser?
+
+?>
---
+++ week10/directory_traversal_part2.php
@@ -0,0 +1,18 @@
+<?php
+
+error_reporting(E_ALL);
+
+$path=basename(realpath($_GET['q']) ? $_GET['q'] : '.');
+
+if ( file_exists( $path ) && is_dir( $path ) ) {
+    print "<pre>";
+    print_r(scandir($path));
+    print "</pre>";
+}
+
+//BONUS
+// if (strpos($_GET['q'],".") !== false){
+//     header("HTTP/1.0 403 Forbidden",true);
+// }
+
+?>
